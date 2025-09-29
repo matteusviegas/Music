ee4ad0592210065b624ac4043c3cfa2831e83312
Matteus VIegas <msmatteusbr2@gmail.com>
2025-09-29 16:31:10 -0300

    add Podcast and music in class pricipal

--- src/br/com/mymusic/principal/Principal.java
+++ src/br/com/mymusic/principal/Principal.java
@@ -8,7 +8,44 @@ public class Principal {
 
     public static void main(String[] args) {
 
+            Musica myMusic = new Musica();
+            myMusic.setAlbum("Michael jackson ");
+            myMusic.setTitulo("Billie Jean");
+            myMusic.setArtista("Michael Jackson");
+            myMusic.setAlbum("Thriller");
+            myMusic.setAnoLancamento(1982);
+            myMusic.setGenero("Pop");
+            myMusic.setDuracao(4.54);
+
+
+            for (int i = 0; i < 1000; i++) {
+                myMusic.reproduzir();
+            }
+
+            for (int i = 0; i < 50; i++) {
+                myMusic.curtir();
+            }
+
+
+            Podcast myPodcast = new Podcast();
+            myPodcast.setTitulo("Flow Podcast");
+            myPodcast.setApresentador("Igor 3K");
+            myPodcast.setConvidados("Monark");
+            myPodcast.setDuracao(145); // em minutos
+            myPodcast.setClassificacao(16);
+
+
+        for (int i = 0; i < 2000; i++) {
+            myPodcast.reproduzir();
+
+        }
+        for (int i = 0; i < 1000; i++) {
+            myPodcast.curtir();
+
+        }
+
     }
+
     }
 
 
